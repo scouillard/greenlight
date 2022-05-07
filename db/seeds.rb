d186351e9dc05e5abc26281edd2822918bb78f8c
scouillard <couillard.samuel@gmail.com>
2022-05-06 22:03:58 -0400

    Add seeds to create 10 Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+10.times do
+  User.create!(
+    email: Faker::Internet.email,
+    name: Faker::Name.name,
+    password: '12345678',
+    password_confirmation: '12345678',
+    provider: 'greenlight',
+  )
+end
+
+
